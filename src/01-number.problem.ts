8ff0a592956d31cee2535776c4ce33303cf79267
Aryan Prince <aryanprincesanty@icloud.com>
2022-12-06 16:10:04 +0300

    test

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 
 export const addTwoNumbers = (a: number, b: number) => {
-  return a + b; // To be implemented 1
+  return a + b; // To be implemented
 };
 
 it("Should add the two numbers together", () => {
