30a0a31a03c55fdbdf10e34e25d381affcd36b37
Aryan Prince <aryanprincesanty@icloud.com>
2022-12-09 17:24:07 +0300

    solved q12

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,11 +1,16 @@
-import { expect, it } from "vitest";
+import { expect, it } from "vitest"
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (
+  amount: number | { value: number; optional?: string }
+) => {
+  if (typeof amount === "object") return amount.value
+  else return amount
+}
 
 it("Should return the amount when passed an object", () => {
-  expect(coerceAmount({ amount: 20 })).toEqual(20);
-});
+  expect(coerceAmount({ value: 20 })).toEqual(20)
+})
 
 it("Should return the amount when passed a number", () => {
-  expect(coerceAmount(20)).toEqual(20);
-});
+  expect(coerceAmount(20)).toEqual(20)
+})
