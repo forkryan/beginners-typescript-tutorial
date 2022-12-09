9981adfcd7f7cbbe6664a27ffcf96fa80846545d
Aryan Prince <aryanprincesanty@icloud.com>
2022-12-09 17:24:28 +0300

    solved q13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -1,15 +1,15 @@
-import { expect, it } from "vitest";
+import { expect, it } from "vitest"
 
 const tryCatchDemo = (state: "fail" | "succeed") => {
   try {
     if (state === "fail") {
-      throw new Error("Failure!");
+      throw new Error("Failure!")
     }
   } catch (e) {
-    return e.message;
+    if (e instanceof Error) return e.message // instanceof picks up any subclass of Error class but 'e: Error' would only Type check for exactly Error class, thanks ChatGPT for the explanation
   }
-};
+}
 
 it("Should return the message when it fails", () => {
-  expect(tryCatchDemo("fail")).toEqual("Failure!");
-});
+  expect(tryCatchDemo("fail")).toEqual("Failure!")
+})
